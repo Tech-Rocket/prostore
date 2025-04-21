228d07ce0ca2c77825def09ac6dbda42f1eae28b
Tech-Rocket <beelawrence8@gmail.com>
2025-04-21 21:19:14 +0000

    Added the loading page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,6 @@
-export default function Home() {
-  return (
-   <h1>
-      Prostore
-   </h1>
-  );
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default async function HomePage() {
+  await delay(2000);
+  return <h1>Prostore</h1>;
 }

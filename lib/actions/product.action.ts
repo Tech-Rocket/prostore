c970e7f57fee72dabce0027043217ed3c1937095
Tech-Rocket <beelawrence8@gmail.com>
2025-05-18 20:13:07 +0000

    Added the prisma.ts file wihin  the db folder

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,13 +1,11 @@
 "use server";
 
-import { PrismaClient } from "../generated/prisma";
+import { prisma } from "@/db/prisma";
 import { convertToPlainObj } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // get latest product
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
